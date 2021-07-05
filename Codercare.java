af588699a6f165968237fdcbc6a55214d004f89c
Sayan Dey <85326550+thesayandey@users.noreply.github.com>
2021-07-05 12:18:49 +0530

    Initial commit

---
+++ Codercare.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import javax.swing.JFrame;
+//import java.util.Scanner;
+
+class Codercare
+{
+    public static void main(String args[])  throws IOException
+    {
+        //System.out.println("Hello World");
+        JFrame myframe = new JFrame();  //creaing an object of the JFrame class
+        myframe.setSize(700,500);
+        myframe.setVisible(true);   //making the frame visible
+        
+    }
+}
+
